fee755a37e01f9d4235bca56886592d2f842da97
Minhkhoa2309 <63503994+Minhkhoa2309@users.noreply.github.com>
2022-09-16 16:30:35 +0700

    Books Create-Index-New

---
+++ models/book.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const path = require('path')
+const coverImageBasePath = 'uploads/bookCover';
+
+
+const bookSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String
+    },
+    pageCount:{
+        type: Number,
+        required: true
+    },
+    publishDate:{
+        type: Date,
+        required: true
+    },
+    createAt:{
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    coverImageName:{
+        type: String,
+        required: true
+    },
+    author:{
+        type: mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'Author'
+    }
+})
+
+bookSchema.virtual('coverImagePath').get(function(){
+    if (this.coverImageName != null){
+        return path.join('/', coverImageBasePath, this.coverImageName)
+    }
+})
+
+module.exports = mongoose.model('Book', bookSchema)
+module.exports.coverImageBasePath = coverImageBasePath
